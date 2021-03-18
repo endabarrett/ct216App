bd87836b58925fc69da3df84ecaf5f80cc5d1ec5
Enda Barrett <barrettenda@gmail.com>
2021-03-18 12:23:58 +0000

    Registration

--- functions/index.js
+++ functions/index.js
@@ -5,67 +5,57 @@ const cors = require('cors')({origin:true});
 admin.initializeApp();
 
 exports.postcomments = functions.https.onRequest((request, response) => {
-   // 1. Receive comment data in here from user POST request
-   // 2. Connect to our Firestore database
-	cors(request, response, () => {
+    // 1. Receive comment data in here from user POST request
+    // 2. Connect to our Firestore database
+    cors(request, response, () => {
 
-		const currentTime = admin.firestore.Timestamp.now();
-		request.body.timestamp = currentTime;
+        const currentTime = admin.firestore.Timestamp.now();
+        request.body.timestamp = currentTime;
 
-		return admin.firestore().collection('comments').add(request.body).then((snapshot) => {
-			response.send("Saved in the database");
-		});
-	});
+        return admin.firestore().collection('comments').add(request.body).then((snapshot) => {
+            response.send("Saved in the database");
+        });
+    });
 });
 
 exports.getcomments = functions.https.onRequest((request, response) => {
 
-	cors(request, response, () => {
-		// 1. Connect to our Firestore database
-		let myData = [];
-		admin.firestore().collection('comments').orderBy("timestamp").get().then((snapshot) => {
+    cors(request, response, () => {
+        // 1. Connect to our Firestore database
+        let myData = [];
+        admin.firestore().collection('comments').orderBy("timestamp").get().then((snapshot) => {
 
-			if (snapshot.empty) {
-				console.log('No matching documents.');
-				response.send('No data ');
-				return;
-			}
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                response.send('No data ');
+                return;
+            }
 
-			snapshot.forEach(doc => {
-				let docObj = {};
-				docObj.id = doc.id;
-				myData.push(Object.assign(docObj, doc.data()));
-			});
+            snapshot.forEach(doc => {
+                let docObj = {};
+                docObj.id = doc.id;
+                myData.push(Object.assign(docObj, doc.data()));
+            });
 
-			// 2. Send data back to client
-			response.send(myData);
-		})
-	});
+            // 2. Send data back to client
+            response.send(myData);
+        })
+    });
 });
-
-exports.login = functions.https.onRequest((request, response) => {
-	cors(request, response, () => {
-		// function body here - use the provided req and res from cors
-			response.send("Hello World");
-	});
-});
-
 exports.updatecomment = functions.https.onRequest((request, response) => {
-	cors(request, response, () => {
-		// function body here - use the provided req and res from cors
-		admin.firestore().collection("comments").doc(request.query.id).update(request.body).then(function() 	{
-			response.send("Document successfully updated!");
-		})
-	});
+    cors(request, response, () => {
+        // function body here - use the provided req and res from cors
+        admin.firestore().collection("comments").doc(request.query.id).update(request.body).then(function() 	{
+            response.send("Document successfully updated!");
+        })
+    });
 });
 
 exports.deletecomment = functions.https.onRequest((request, response) => {
-	cors(request, response, () => {
-		// your function body here - use the provided req and res from cors
-		admin.firestore().collection("comments").doc(request.query.id).delete().then(function() 	{
-			response.send("Document successfully deleted!");
-		})
-	});
+    cors(request, response, () => {
+        // your function body here - use the provided req and res from cors
+        admin.firestore().collection("comments").doc(request.query.id).delete().then(function() 	{
+            response.send("Document successfully deleted!");
+        })
+    });
 });
-
-
